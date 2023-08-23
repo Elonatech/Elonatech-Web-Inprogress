fd30e0226a71464d5981497bd1feb6f72325805d
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-08-23 16:04:52 +0100

    update marketing

--- src/pages/marketing/ppc/ppc.jsx
+++ src/pages/marketing/ppc/ppc.jsx
@@ -1,33 +1,40 @@
-import gh  from './captions/about-us.png'
+import aboutImage  from './captions/about-us.png'
 import './ppc.css'
 
 const Ppc = () => {
     return (
     <>
 {/* header */}
-<div
- class="container-fluid page-header bg-dark py-5 mb-5"
-  style={{height:"500px"}}
->
- <div class="container py-5 ">
-   <h1 class=" mt-5 text-white">We Are A Full-Service Digital Marketing <br /> Agency In Lagos, Nigeria</h1>
-<p  class="text-white pt-2">We Provides Round-The-Clock Maintenance for All Network Equipment</p>
-<p  class="text-white pt-2">we are committed to supplying you with the most reliable advice, support and services at all times, wherever you are in Nigeria.</p>
- <p class="mb-0 pt-2"><a href="#" class="btn btn-success py-3 px-4">Contact Us</a></p>
- </div>
-</div>    
+<section class="mt-5" style={{backgroundColor:"#1B4D3E"}}> 
+      <div class="container" >
+        <div class="row justify-content-between">
+          <div class="col-md-8 col-lg-8 d-flex align-items-center">
+            <div class="w-100 py-4" style={{marginTop:"100px"}}>
+              <h2 class="text-white">We Provides Round-The-Clock Maintenance for All Network Equipment</h2>
+              <p  class="text-white pt-2">We Provides Round-The-Clock Maintenance for All Network Equipment</p>
+              <p  class="text-white pt-2">we are committed to supplying you with the most reliable advice,
+                             support and services at all times, wherever you are in Nigeria.</p>
+              <p class="mb-0 pt-2"><a href="#" class="btn btn-success py-3 px-4">Contact Us</a></p>
+            </div>
+          </div>
+          <div class="col-md-4 col-lg-4 d-flex align-items-end">
+             <img src="img/" class="img-fluid" alt=""/> 
+          </div>
+        </div>
+      </div>
+    </section>
 
 
-{/* faiejfc */}
-<section class="about-section spad">
+{/* ppc */}
+<section class="about-section spad mt-5">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="about__img">
-                        <img src={gh} alt=""/>
+                <div class="col-md-6">
+                    <div class="">
+                        <img src={aboutImage} className='img-fluid' alt=""/>
                     </div>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-md-6">
                     <div class="about__text">
                         <h2>Welcom to Deerhost</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
@@ -35,7 +42,7 @@ const Ppc = () => {
                             viverra maecenas accumsan lacus vel facilisis.</p>
                         <div class="about__achievement">
                             <div class="about__achieve__item">
-                                <span class="fa fa-user-o"></span>
+                                <span class="fa fa-clone"></span>
                                 <h4 class="achieve-counter">2468</h4>
                                 <p>Clients</p>
                             </div>
@@ -55,7 +62,7 @@ const Ppc = () => {
                                 <p>Installs</p>
                             </div>
                         </div>
-                        <a href="#" class="btn btn-primary"><b>Get started now</b></a>
+                        <a href="#" class="btn btn-primary mb-4"><b>Get started now</b></a>
                     </div>
                 </div>
             </div>
