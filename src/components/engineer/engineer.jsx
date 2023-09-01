698054f9d791f55b595e91692ac6cd638bf3e4cb
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-09-01 17:28:07 +0100

    update main

--- src/components/engineer/engineer.jsx
+++ src/components/engineer/engineer.jsx
@@ -1,59 +1,99 @@
-import img from './about-2.jpg'
-import pro from './author.png'
+// import img from './captions/about-2.jpg'
+// import pro from './captions/author.png'
+import computerHeader from './captions/3945114.jpg'
+import computerRepair from './captions/computer repair.jpg'
 import './eng.css'
 const Engineer = () => {
     return (
 <>
 
 {/* header */}
-<div
- class="container-fluid  bg-secondary py-5 mb-5"
-  style={{height:"500px"}}
->
- <div class="container py-5 ">
-   <h1 class=" mt-5 text-white text-center">Computer Engineering</h1>
-   <h3 class=" mt-2 text-white text-center">Because your business works when your hardware works.</h3>
-   <h3 class=" mt-2 text-white text-center">While computer hardware configurations vary widely, we work on a broad range of hardware systems.</h3>
+<div class="container-fluid   py-5 mb-5" style={{backgroundImage:`url(${computerHeader})`, backgroundPosition:"center", backgroundRepeat:'no-repeat', backgroundSize:"cover"}} >
+ <div class="container py-5 text-center ">
+   <h1 class=" mt-5  py-3 text-white">Computer Engineering</h1>
+   <h5 class=" mt-2  py-3 text-white ">Because your business works when your hardware works.</h5>
+   <h6 class=" mt-2 py-3 text-white">While computer hardware configurations vary widely, we work on a broad range of hardware systems.</h6>
+
+   {/* <div className="text-center">
+   <ul className='d-flex'>
+    <li className='text-white' >Client Computer Systems (Desktop & Laptop PCs)</li>
+    <li className='text-white'> Servers & Workstations</li>
+   </ul>
+   </div> */}
+
  </div>
 </div>
 
+
+
+
+
+
+
 {/* body */}
- <div className="container-fluid mb-5">
-<div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <p className='mt-3 fs-4'>For specialized hardware services, we have highly skilled full-time computer hardware techs who can fix all types of hardware problems including power jacks, keyboards, screens, fans, overheating issues, liquid spills, soldering jobs, loose cabling, and replacement of motherboards and discrete video cards.
-         While computer hardware configurations vary widely, we work on a broad range of hardware systems.</p>
-            <ul className='fs-4'>
-            <li>Client Computer Systems (Desktop & Laptop PCs)</li>
-            <li>Servers & Workstations</li>
+ <div className="container-fluid  mb-5">
+<div class="row justify-content-md-center ">
+      <div class="col-md-6 order-md-2  ">
+        <p className='mt-3 fs-5'>For specialized hardware services, we have highly skilled full-time computer hardware techs who can fix all types of hardware problems including power jacks, keyboards, screens, fans, overheating issues, liquid spills, soldering jobs, loose cabling, and replacement of motherboards and discrete video cards.
+              While computer hardware configurations vary widely, we work on a broad range of hardware systems.</p>
+            <ul className='fs-5  list-unstyled'>
+            <li><i class="bi bi-record-circle me-3"></i>Client Computer Systems (Desktop & Laptop PCs)</li>
+            <li><i class="bi bi-record-circle me-3"></i>Servers & Workstations</li>
         </ul>
-      </div>
+      </div>  
+      
       <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+        <img src={computerRepair} alt=""   className='img-fluid rounded  shadow' />
       </div>
     </div>
 </div>
 
+
+
+
+
+
+
+
 {/* Desktop */}
 <section class="bg-light mb-4" >
 			<div class="container-fluid">
-				<div class="row justify-content-between">
-					<div class="col-md-8 col-lg-8 d-flex align-items-center">
-						<div class="w-100 py-4">
+				<div class="row ">
+					<div class="col-md-6 col-lg-6 d-flex">
+						<div class="">
 							<h2   class="text-dark fs-1 mb-4">Desktop and Laptop PCs</h2>
-							<p  class="text-dark fs-4">We offer all kinds of laptop, desktop and server repairs of all makes and models, including Windows, Mac and Linux computers. We do virus removal without reformatting so you don't lose your important files or photos. We repair both hardware and software. We have no queue so come any time and we'll start fixing your computer right away.</p>
+							<p  class="text-dark">We offer all kinds of laptop, desktop and server repairs of all makes and models, including Windows, Mac and Linux computers. We do virus removal  <br /> without reformatting so you don't lose your important files or photos. We repair both hardware and software. We have no queue so come any time and we'll start fixing your computer right away.</p>
 						</div>
 					</div>
-					<div class="col-md-4 col-lg-4 d-flex align-items-end ">
-						<img src={img} class="img-fluid" alt=""/>
+					<div class="col-md-6 col-lg-6 d-flex align-items-end ">
+            {/* <ul className='list-unstyled'>
+              <li><img src={img} class="img-fluid" className='' style={{ paddingLeft:"215px"}} alt=""/></li>
+              <li><img src={img} class="img-fluid"  className='' style={{}} alt=""/></li>
+            </ul> */}
+			        <div className="row">
+                {/* <div className="col">  <div className="card">people1</div></div>
+                <div className="coi">  <div className="card">people2</div></div> */}
+                <div className="col"><div className="card  p-5" style={{marginTop:"130px"}}>people3</div></div>
+                <div className="col">  <div className="card   p-5">people4</div></div>
+              </div>
+          
+          
+            
+           
+
+
 					</div>
 				</div>
 			</div>
 </section> 
 
 
+
+
+
 {/* convenient */}
-<div className="container mb-5"> 
+
+{/* <div className="container mb-5"> 
 <div class="row">
       <div class="col-md-7 order-md-4">
         <h2   class="text-dark fs-1 mb-4">We offer 4 Convenient Computer Repair Options</h2>
@@ -69,26 +109,41 @@ const Engineer = () => {
       <img src={img} class="img-fluid" alt=""/>
       </div>
     </div>
-</div>
+</div> */}
 
+<div className="container-fluid  mt-5 mb-5">
+<div class="row justify-content-md-center ">
+      <div class="col-md-6 order-md-2  mt-5 ">
+        <h1>We offer 4 Convenient Computer Repair Options</h1>
+            <ul className='fs-5  list-unstyled '>
+            <li className='kos'><i class="bi bi-check-square fs-4 me-3"></i>Pick-up and Return</li>
+            <li className='kos'><i class="bi bi-check-square fs-4 me-3"></i>Onsite Repair</li>
+            <li className='kos'><i class="bi bi-check-square fs-4 me-3"></i>Walk-in and Repair</li>
+            <li className='kos'><i class="bi bi-check-square fs-4 me-3"></i>Remote Support</li>
+            </ul>
+            <h5 className='text-primary'>If you require friendly, expert staff and to have your computer running better than ever, come to our computer repair workshop or give us a call.</h5>
+      </div>  
+      
+      <div class="col-md-5 order-md-1">
+        <img src={computerRepair} alt=""   className='img-fluid rounded  shadow' />
+      </div>
+    </div>
+</div>
 
 
 {/* contact us */}
 <section class="bg-dark">
-			<div class="container">
-				<div class="row justify-content-between">
-					<div class="col-md-8 col-lg-8 d-flex align-items-center">
+			<div class="container-fluid">
+				<div class="row justify-content-lg-center">
+					<div class="col-md-7 d-flex align-items-center">
 						<div class="w-100 py-4">
-							<h2   class="text-white">Have a project on your mind.</h2>
-							<p  class="text-white">Being online goes beyond social media accounts, you need an actual home on the internet. That’s a website! Here are 6 reasons a website is a must for your business.</p>
-							<p class="mb-0" data-bs-toggle="modal" data-bs-target="#myModal"><a class="btn btn-primary py-3 px-4">Contact me</a></p>
-
-       
-         
+							<h1   class="text-white mb-3">Get Ahead of your Competitors</h1>
+							<p  class="text-white mb-5">If you are ready to become the best in your industry and also stand out from a sea of hungry competitors,  <br />then Elonatech is the partner you need. Get in touch with us below, let's take your busines to the next level.</p>
+            <h3 className='text-white'>Ready to Start your Project? <a class="btn btn-primary ms-4"><h5>Request a Quote</h5></a></h3>    
 						</div>
 					</div>
 					<div class="col-md-4 col-lg-4 d-flex align-items-end">
-						<img src={pro} class="img-fluid" alt=""/>
+						{/* <img src={pro} class="img-fluid" alt=""/> */}
        
 					</div>
 				</div>
--- src/components/printer/printer.jsx
+++ src/components/printer/printer.jsx
@@ -1,14 +1,19 @@
-import printer2 from './printer2.jpg'
+import printer2 from './caption/printer2.jpg';
+import bg2 from './caption/printerImg.jpg';
+import printer4 from './caption/external-printer-home-office-photo3ideastudio-lineal-color-photo3ideastudio.png'
+import printer5 from './caption/external-printer-printing-tulpahn-outline-color-tulpahn-1.png'
+import printerCombine from './caption/printersCombined.png'
+import printerHeader from './caption/wire2.jpg'
 
 const Printer = () => {
     return (
  <>
  {/* header */}
-<div class="container-fluid mb-  bg-secondary py-5 " style={{height:"520x"}}>
+<div class="container-fluid bg-secondary py-5 " style={{backgroundImage:`url(${printerHeader})`, backgroundPosition:"center", backgroundRepeat:'no-repeat', backgroundSize:"cover"}} >
   <div class="container py-5 ">
-    <h1 class=" mt-5 text-white text-center">Printer Repair & Maintenance</h1>
-    <h4 class=" mt-2 text-white text-center">Keeping Up with all Necessary Maintenance is The Best Way to Protect your Investment</h4>
-    <h5 class=" mt-2 text-white text-center">A malfunctioning or out of service printer, copier or multifunction device can be disastrous for your business</h5>
+    <h1 class=" mt-5 text-white text-center pb-5">Printer Repair & Maintenance</h1>
+    <h4 class=" mt-2 text-white text-center ">Keeping Up with all Necessary Maintenance is The Best Way to Protect your Investment</h4>
+    <h6 class=" mt-2 text-white text-center pb-5">A malfunctioning or out of service printer, copier or multifunction device can be disastrous for your business</h6>
     
   </div>
 
@@ -17,62 +22,45 @@ const Printer = () => {
 {/* body one */}
  <div class="container-fluid">
   <div class="container-fluid py-5 ">
-    <h1 class=" mt-5 text-dark text-center">Printer Repair & Maintenance</h1>
-    <p class=" mt-2 fs-4 text-dark text-center">Maintaining the printer and copier in your office is very important. Keeping up with all necessary printer repair and maintenance is the best way to protect your valuable investment. It also ensures that your business will remain productive and avoid frustrating delays due to printer malfunction</p>
-    <p class=" mt-2 fs-4  text-dark text-center">We offer a full range of printer repair services. Do you have issues with your printers or copiers? Is your printer or photocopier giving you a hard time? Your printing jobs should be more straight forward and seamless. Let's FIX IT!!
+    <h1 class=" mt-5 text-center" style={{color:"#34548c"}}>Printer Repair & Maintenance</h1>
+    <p class="text-dark fs-5 text-center">Maintaining the printer and copier in your office is very important. Keeping up with all necessary printer repair and maintenance is the best way to protect your valuable investment. It also ensures that your business will remain productive and avoid frustrating delays due to printer malfunction</p>
+    <p class=" mt-2 fs-5  text-dark text-center">We offer a full range of printer repair services. Do you have issues with your printers or copiers? Is your printer or photocopier giving you a hard time? Your printing jobs should be more straight forward and seamless. Let's FIX IT!!
 </p>   
 </div>
- </div>
+ </div> 
 
  {/* cards */}
-
  <div class="album py-5">
-      <div class="container bg-primary">
-
+      <div class="container rounded" style={{backgroundColor:"#34548c"}}>
         <div class="row row-cols-1 row-cols-lg-2 row-cols-lg-3 g-3">
           <div class="col">
-            <div class="card   shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card shadow-sm" style={{height:"380px"}}>
+              <div className="text-center mt-2">
+              <img src={printer4} className='card-img-top text-center' style={{height:"4rem" , width:"4rem"}} alt="" />
+              </div>
               <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-body-secondary">9 mins
-                  </small>
-                </div>
+                <h5 className='text-center'>Repair and Maintenance Services for out of Warranty Print Products</h5>
+                <p  class="text-center fs-6 mb-4">Highly aware of your company’s business needs, 
+                Elonatech provides a portfolio of services that can be ordered as the occasion demands.
+                 Your printer, photocopier or scanner is down, and you are out of warranty, or your device is not even covered by a warranty. 
+                You can opt for our repair option which includes the repair service and part replacement that can be managed throughout the usage of the device.</p>
               </div>
             </div>
           </div>
           <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-body-secondary">9 mins</small>
-                </div>
-              </div>
-            </div>
+          <div className="card" style={{height:"380px" , backgroundImage:`url(${bg2})` , backgroundRepeat:"no-repeat", backgroundSize:"cover" }}></div>
           </div>
           <div class="col mb-3">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <div class="card shadow-sm" style={{height:"380px"}}>
+              <div className="text-center mt-4">
+              <img src={printer5} className='card-img-top text-center' style={{height:"4rem" , width:"4rem"}} alt="" />
+              </div>
               <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-body-secondary">9 mins</small>
-                </div>
+                <h5 className='text-center'>Printer installation and Configuration</h5>
+                <p  class="text-center fs-6 mb-4">Our Printer Repair Technicians is highly skilled and 
+                 proficient in the regular and routine repairs, 
+                including replacements, installation and
+                 configuration of new printers, maintaining and ensuring the proper functioning of standalone printers, scanners or Network Printers.</p>
               </div>
             </div>
           </div>
@@ -83,7 +71,7 @@ const Printer = () => {
   </div>
 
  {/* printer repair */}
-  <div className="container mb-5"> 
+{/* <div className="container mb-5"> 
 <div class="row">
       <div class="col-md-7 order-md-4">
         <h2   class="text-dark fs-1 mb-4">We offer 4 Convenient Computer Repair Options</h2>
@@ -96,10 +84,37 @@ const Printer = () => {
 		<p class="text-dark fs-4">If you require friendly, expert staff and to have your computer running better than ever, come to our computer repair workshop or give us a call.</p>
       </div>
       <div class="col-md-5 ">
-      {/* <img src={printer2} class="img-fluid" alt=""/> */}
-      <img src={printer2}  class="img-fluid" alt="" />
+      <img src={printer2} class="img-fluid" alt=""/>
       </div>
     </div>
+</div> */}
+
+
+{/* specialists */}
+<div className="container-fluid" style={{backgroundColor:"#34548c"}}>
+  <div className="row justify-content-md-center ">
+  <div className="col-lg-5">
+    <div className="card  p-3 mt-4" style={{border:"none" , backgroundColor:"#34548c"}}>
+      <h4 className='text-white'>Our Printer Specialists Work on The Following Top Brands</h4>
+      <ul className='list-unstyled text-white'>
+        <li><h5><i class="bi bi-printer text-danger me-3"></i>Brother</h5></li>
+        <li><h5><i class="bi bi-printer-fill text-danger me-3"></i>Canon</h5></li>
+        <li><h5><i class="bi bi-printer text-danger me-3"></i>Epson</h5></li>
+        <li><h5><i class="bi bi-printer-fill text-danger me-3"></i>HP (Hewlett Packard)</h5></li>
+        <li><h5><i class="bi bi-printer text-danger me-3"></i>Lexmark</h5></li>
+        <li><h5><i class="bi bi-printer-fill text-danger me-3"></i>Ricoh</h5></li>
+        <li><h5><i class="bi bi-printer text-danger me-3"></i>Samsung</h5></li>
+        <li><h5><i class="bi bi-printer-fill text-danger me-3"></i>Sharp</h5></li>
+        <li><h5><i class="bi bi-printer text-danger me-3"></i>Xerox</h5></li>
+      </ul>
+    </div>
+  </div>
+  <div className="col-lg-6">
+    <div className="card" style={{border:"none" , backgroundColor:"#34548c"}}>
+      <img src={printerCombine} alt="" />
+    </div>
+  </div>
+  </div>
 </div>
 
  </>
