a3d503f5d23c97f970e52b0a32e1edb514fb7c8c
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-10-05 14:59:21 +0100

    update pages

--- src/pages/marketing/seo/seo.jsx
+++ src/pages/marketing/seo/seo.jsx
@@ -6,7 +6,7 @@ const Seo = () => {
     return (
  <>
 {/* Header */}
-<div class="container-fluid bg-secondary  py-5 " style={{height:"500px" , marginBottom:"120px" , backgroundImage:`linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${seoHeader})`, backgroundRepeat:"no-repeat" , backgroundPosition:"center", backgroundSize:"cover"}}>
+<div class="container-fluid bg-secondary  py-5 " style={{height:"500px" , backgroundImage:`linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${seoHeader})`, backgroundRepeat:"no-repeat" , backgroundPosition:"center", backgroundSize:"cover"}}>
  <div class="py-5 mt-5 "> 
    <h2 class=" mt-5 text-white text-center">Seo Marketing</h2>
    <h5 class=" mt-4 text-white text-center">Experience the Next Level of Internet Speed and Reliability</h5>
@@ -14,16 +14,36 @@ const Seo = () => {
  </div>
 </div>
 
-{/* body */}
+{/* D */}
 
-<div class="section-top-border" style={{marginBottom:"120px"}} >
+<div class="section-top-border mt-5">
+<div className=''></div>
+<div className="container ">
+<div class="row">
+<h3>Our Services</h3>
+	<div class="col-md-6">
+	<p>Victorious stands apart from other SEO firms because we approach organic search traffic as a powerful marketing channel in its own right. 
+	<br /><br />
+	 Our rigorous process draws from six areas of discipline to optimize and amplify organic traffic into a strategic pipeline of prospective customers.
+    </p>
+	</div>
+	<div class="col-md-6">
+	<p>Explore our SEO services page to find a 12-month campaign schedule and a complete list of services you can expect from an SEO expert agency like Victorious.
+	<br /><br />
+	</p>
+   </div>
+</div>
+</div>
+</div>
+
+<div class="section-top-border mt-5" style={{marginBottom:"120px"}} >
 <div className=''></div>
 <div className="container ">
 <div class="row">
 	<div class="col-md-6">
-		<h3>How Seo works</h3>
+	<h3>How Seo works</h3>
 	<p>Recently, the US Federal government banned online casinos from operating in America by making  Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-		Quos corporis explicabo esse consequuntur dolorem praesentium magnam eligendi natus unde nulla. Perspiciatis officiis minus rem neque, adipisci quasi laborum nisi quam!</p>
+	Quos corporis explicabo esse consequuntur dolorem praesentium magnam eligendi natus unde nulla. Perspiciatis officiis minus rem neque, adipisci quasi laborum nisi quam!</p>
 	</div>
 	<div class="col-md-6">
 	<img src={seoImg1} alt="" class="img-fluid"/>
@@ -32,20 +52,92 @@ const Seo = () => {
 </div>
 </div>
 
+{/* F */}
+<div className="container mt-5 mb-5">
+    <div className="row">
+        <div className="col-md-6">
+        <ul className='list-unstyled'>
+        <li>
+            <div className='d-flex'>
+            <i class="bi bi-check-circle-fill fs-4"  ></i>
+            <h6 className='ms-3 mt-2 fw-bold'> Display Ads</h6>
+            </div>
+            <p className='lead fs-6'> <strong className='fw-bold'>Benefit:</strong> Display ads include banner, image, or interactive ads that are shown on various websites within the ad network. They provide a broader reach, increasing brand awareness and visibility.
+            <br />
+            <strong className='fw-bold'>Best for:</strong> Building brand awareness, retargeting website visitors, and promoting visual content like infographics and videos.
+            </p>
+        </li>
+        <li>
+            <div className='d-flex'>
+            <i class="bi bi-check-circle-fill fs-4"></i>
+            <h6 className='ms-3 mt-2 fw-bold' >Shopping Ads</h6>
+            </div>
+            <p className='lead fs-6'><strong className='fw-bold'>Benefit:</strong>  Shopping ads are product-based ads that show images, prices, and other relevant information directly on search engine results. They are particularly effective for e-commerce businesses, showcasing products and attracting users with high purchase intent.
+            <br />
+            <strong className='fw-bold'>Best for:</strong> E-commerce websites looking to promote specific products and drive sales.
+            </p>
+        </li>
+        <li>
+            <div className='d-flex'>
+            <i class="bi bi-check-circle-fill fs-4"></i>
+            <h6 className='ms-3 mt-2 fw-bold' >Video Ads</h6>
+            </div>
+            <p className='lead fs-6'><strong className='fw-bold'>Benefit:</strong>  Video ads are shown on platforms like YouTube and can deliver engaging content to captivate the audience. They are highly effective for storytelling and brand messaging.
+            <br />
+            <strong className='fw-bold'>Best for:</strong> Brand awareness, storytelling, and showcasing products or services through visual media.
+            </p>
+        </li>
+        </ul>
+   </div>
+        <div className="col-md-6">
+        <ul className='list-unstyled'>
+        <li>
+            <div className='d-flex'>
+            <i class="bi bi-check-circle-fill fs-4"></i>
+            <h6 className='ms-3 mt-2 fw-bold' > Remarketing/Retargeting Ads</h6>
+            </div>
+            <p className='lead fs-6'><strong className='fw-bold'>Benefit:</strong> Remarketing ads target users who have previously visited your website but didn't convert. They remind potential customers of your brand, increasing the chances of conversion.
+            <br />
+            <strong className='fw-bold'>Best for:</strong> Converting bounced website visitors, reinforcing brand messaging, and driving previous visitors back to the site.
+            </p>
+        </li>
+        <li>
+            <div className='d-flex'>
+            <i class="bi bi-check-circle-fill fs-4"></i>
+            <h6 className='ms-3 mt-2 fw-bold' >Social Media Ads</h6>
+            </div>
+            <p className='lead fs-6'><strong className='fw-bold'>Benefit:</strong>  Social media ads are displayed on social platforms (e.g., Facebook, Instagram, Twitter) and leverage detailed user data for precise targeting. They enable businesses to reach their target audience based on demographics, interests, and behaviors.
+            <br />
+            <strong className='fw-bold'>Best for:</strong> Building brand awareness, lead generation with specific user segments.
+            </p>
+        </li>
+        <li>
+            <div className='d-flex'>
+            <i class="bi bi-check-circle-fill fs-4"></i>
+            <h6 className='ms-3 mt-2 fw-bold' >App Install Ads</h6>
+            </div>
+            <p className='lead fs-6'><strong className='fw-bold'>Benefit:</strong>  App install ads aim to drive installations of mobile applications. They often appear on mobile devices within other apps or as sponsored content in app stores.
+            <br />
+            <strong className='fw-bold'>Best for:</strong> Mobile app developers looking to increase app downloads and user acquisition.
+            Each type of PPC campaign offers unique advantages, and the effectiveness of a campaign depends on the advertiser's specific goals, target audience, and budget. Combining different PPC strategies can create a well-rounded digital advertising approach to achieve various marketing objectives.
+            </p>
+        </li>
+        </ul>   
+        </div>
+    </div>
+</div> 
 
-{/* image 2  */}
-
+{/* E */}
 <div class="section-top-border mt-5">
 <div className="container ">
 <div class="row">
-
   <div class="col-md-6">
 	<img src={seoImg2} alt="" class="img-fluid"/>
 	</div>
 	<div class="col-md-6">
-		<h3>How Seo works</h3>
+	<h3>How Seo works</h3>
 	<p>Recently, the US Federal government banned online casinos from operating in America by making  Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-		Quos corporis explicabo esse consequuntur dolorem praesentium magnam eligendi natus unde nulla. Perspiciatis officiis minus rem neque, adipisci quasi laborum nisi quam!</p>
+	Quos corporis explicabo esse consequuntur dolorem praesentium magnam eligendi natus unde nulla. Perspiciatis officiis minus rem neque, adipisci quasi laborum nisi quam!</p>
 	</div>
 </div>
 </div>
